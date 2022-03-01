7aa725edd9604b338e0d78a22e5dfcf7fe7c2f23
imrancu <mdimran3772@gmail.com>
2022-03-02 00:28:57 +0600

    show detail done

--- js/app.js
+++ js/app.js
@@ -3,7 +3,6 @@ const discoverPhone = () => {
     const discoverText = discoverPhone.value;
     // Empty input field 
     discoverPhone.value = '';
-
     const url = `https://openapi.programming-hero.com/api/phones?search=${discoverText}`;
     fetch(url)
         .then(res => res.json())
@@ -11,7 +10,8 @@ const discoverPhone = () => {
 }
 const showDiscoveredPhones = phones => {
     const searchResult = document.getElementById('search-result');
-    // console.log(phones)
+    // Previous Data will be disappeared
+    searchResult.textContent = '';
     phones.slice(0, 20).forEach(phone => {
         const div = document.createElement('div');
         div.classList.add('col');
@@ -22,18 +22,39 @@ const showDiscoveredPhones = phones => {
                     <h3 class="card-title">${phone.phone_name}</h3>
                     <h5>${phone.brand}</h5>
                 </div>
-                <button class="text-white btn btn-success" onclick="showPhoneDetails('${phone.slug}')">Details</button>
+                <button class="text-white btn btn-success" onclick="phoneDetails('${phone.slug}')">Details</button>
             </div>
         `;
         searchResult.appendChild(div);
     })
 }
 
-const showPhoneDetails = phoneId => {
+const phoneDetails = phoneId => {
     console.log(phoneId);
     const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data.data.mainFeatures));
+        .then(data => showPhoneDetails(data.data));
 
+};
+const showPhoneDetails = phone => {
+    console.log(phone);
+    const singlePhoneDetails = document.getElementById('single-phone-details');
+    const div = document.createElement('div');
+    div.classList.add('card', 'px-5', 'mb-4', 'mx-auto');
+    div.innerHTML = `
+    <img src="${phone.image}" class="card-img-top w-25 mx-auto pt-4" alt="photo" />
+    <div class="card-body">
+        <h5>${phone.releaseDate?phone.releaseDate:"Release Date is empty!"}</h5>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">${phone.mainFeatures.chipSet}</li>
+            <li class="list-group-item">${phone.mainFeatures.displaySize}</li>
+            <li class="list-group-item">${phone.mainFeatures.memory}</li>
+            <li class="list-group-item">${phone.mainFeatures.memory}</li>
+            <li class="list-group-item">${phone.mainFeatures.storage}</li>
+            <li class="list-group-item">${phone.mainFeatures.sensors[0]}</li>
+        </ul>
+    </div>
+    `;
+    singlePhoneDetails.appendChild(div);
 };
